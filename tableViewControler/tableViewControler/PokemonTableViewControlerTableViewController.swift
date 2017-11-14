32183bde0f1cfe1cb113639132d4917a73b73975
niksolaz <solazzo.nicola@gmail.com>
2017-11-14 18:24:17 +0100

    Added sequenze page e return button

--- tableViewControler/tableViewControler/PokemonTableViewControlerTableViewController.swift
+++ tableViewControler/tableViewControler/PokemonTableViewControlerTableViewController.swift
@@ -45,6 +45,9 @@ class PokemonTableViewControlerTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "pokDetail", sender: nil)
+    }
 
     /*
     // Override to support conditional editing of the table view.
