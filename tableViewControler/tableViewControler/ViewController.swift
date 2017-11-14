7fe7ab013726ba3e5924225593d86e5f1295c504
niksolaz <solazzo.nicola@gmail.com>
2017-11-14 18:32:51 +0100

    Dismiss page name pokemon

--- tableViewControler/tableViewControler/ViewController.swift
+++ tableViewControler/tableViewControler/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didClickBack(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+        //performSegue(withIdentifier: "backToPokemonlist", sender: nil)
+    }
+    
 }
 
